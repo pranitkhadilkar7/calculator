a5b96829b8cac28ae5fef3454d5cd28d42eda357
Pranit Khadilkar <pranit@emtech.com>
2024-11-17 19:23:53 +0530

    added code to create latex with nested exponential

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -2,11 +2,48 @@ export function createLatexString(formula: string) {
   // remove all whitespaces
   formula = formula.replace(/\s+/g, "");
   // replace all ^ with <sup>
-  formula = formula.replace(/\^([a-zA-Z0-9]+)/g, (_, exp) => {
-    return `<sup>${exp}</sup>`;
-  });
+  //   formula = formula.replace(/\^([a-zA-Z0-9])/g, (_, exp) => {
+  //     return `<sup>${exp}</sup>`;
+  //   });
+  formula = generateLatexForExpontent(formula);
   // replace all * with ⋅
   return formula.replace(/\*/g, "⋅");
+}
+
+export function generateLatexForExpontent(formula: string) {
+  let result = "";
+  let i = 0;
+  while (i < formula.length) {
+    const char = formula[i];
+    if (char === "^") {
+      if (formula[i + 1] && /^[a-zA-Z0-9]/.test(formula[i + 1])) {
+        result += `<sup>${formula[i + 1]}</sup>`;
+        i += 2;
+      } else if (formula[i + 2] && formula[i + 1] === "(") {
+        let j = i + 2;
+        let count = 1;
+        while (j < formula.length && count > 0) {
+          if (formula[j] === "(") {
+            count++;
+          } else if (formula[j] === ")") {
+            count--;
+          }
+          j++;
+        }
+        result += `<sup>(${generateLatexForExpontent(
+          formula.substring(i + 2, j - 1)
+        )})</sup>`;
+        i = j;
+      } else {
+        result += char;
+        i++;
+      }
+    } else {
+      result += char;
+      i++;
+    }
+  }
+  return result;
 }
 
 export function getVariablesFromFormula(formula: string) {
@@ -39,9 +76,13 @@ export function isValidFormula(formula: string) {
 
   const invalidPatterns = [
     /[+\-*/^]{2,}/, // two operators together
-    /^[*/^]/, // operator * or ^ at the start
+    /^[*/^]/, // operator *, /, ^ at the start
     /[+\-*/^]$/, // operator at the end
     /\(\)/, // empty ()
+    /[a-zA-Z]{2,3}/, // consecutive variables
+    /[+\-*/^]\)/, // operator followed by )
+    /\([*/^]/, // followed by ( followed by operator *, /, ^
+    /[a-zA-Z0-9]\(/, // variable followed by (
   ];
 
   for (const pattern of invalidPatterns) {
